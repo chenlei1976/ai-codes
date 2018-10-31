18a322b92b8279638665af4e7c961135b198aa81
chenlei1976 <42330800+chenlei1976@users.noreply.github.com>
2018-10-31 16:23:28 +0800

    Update vgg16-keras.py

--- deep_learning/vgg16-keras.py
+++ deep_learning/vgg16-keras.py
@@ -20,136 +20,81 @@ from keras.layers.convolutional import Conv2D, MaxPooling2D, ZeroPadding2D
 
 from keras.optimizers import SGD
 
+tf.flags.DEFINE_integer("NumOfClass", 10, "Number Of Class")
+tf.flags.DEFINE_integer("VGGImageWidth", 224, "VGG Image Width")
+tf.flags.DEFINE_integer("VGGImageHeight", 224, "VGG Image Width")
+tf.flags.DEFINE_integer("BatchSize", 32, "BatchSize")
+tf.flags.DEFINE_string("TrainData", "./data/train", "train data folder")
+tf.flags.DEFINE_string("TestData", "./data/test", "test data folder")
+tf.flags.DEFINE_string("ValidationData", "./data/test", "test data folder")
 
-def vgg16_inner(include_top=False, weights='imagenet', input_shape=(224, 224, 3), frozen=False):
-    vgg16 = VGG16(include_top=include_top, weights=weights, input_shape=input_shape)
+FLAGS = tf.flags.FLAGS
 
-    if frozen:
-        for layer in vgg16.layers:
-            layer.trainable = False
+
+def vgg16Inner(includeTop=False, weights='imagenet', inputShape=(FLAGS.VGGImageHeight, FLAGS.VGGImageWidth, 3),
+               trainableLayer=''):
+
+    vgg16 = VGG16(include_top=includeTop, weights=weights, input_shape=inputShape)
+
+    isTrainable = False
+    for layer in vgg16.layers:
+        if layer.name == trainableLayer: # 'block5_conv1'
+            isTrainable = True
+        layer.trainable = isTrainable
+
     last = vgg16.output
 
     x = Flatten()(last)
-    x = Dense(256, activation='relu')(x)
+    x = Dense(4096, activation='relu')(x)
     x = Dropout(0.5)(x)
-    x = Dense(256, activation='relu')(x)
+    x = Dense(4096, activation='relu')(x)
     x = Dropout(0.5)(x)
-    x = Dense(1, activation='sigmoid')(x)
-
+    if FLAGS.NumOfClass > 2:
+        x = Dense(FLAGS.NumOfClass, activation='softmax')(x)
+    else:
+        x = Dense(FLAGS.NumOfClass, activation='sigmoid')(x)
     model = Model(inputs=vgg16.input, outputs=x)
 
     return model
 
 
-def vgg16_seq(weights_path=None):
-    model = Sequential()
-
-    model.add(ZeroPadding2D((1, 1), input_shape=(224, 224, 3)))
-
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-
-    model.add(ZeroPadding2D((1, 1)))
-
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-    model.add(ZeroPadding2D((1, 1)))
-
-    model.add(Conv2D(128, (3, 3), activation='relu'))
-
-    model.add(ZeroPadding2D((1, 1)))
-
-    model.add(Conv2D(128, (3, 3), activation='relu'))
-
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-    model.add(ZeroPadding2D((1, 1)))
-
-    model.add(Conv2D(256, (3, 3), activation='relu'))
-
-    model.add(ZeroPadding2D((1, 1)))
-
-    model.add(Conv2D(256, (3, 3), activation='relu'))
-
-    model.add(ZeroPadding2D((1, 1)))
-
-    model.add(Conv2D(256, (3, 3), activation='relu'))
-
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-    model.add(ZeroPadding2D((1, 1)))
-
-    model.add(Conv2D(512, (3, 3), activation='relu'))
-
-    model.add(ZeroPadding2D((1, 1)))
-
-    model.add(Conv2D(512, (3, 3), activation='relu'))
-
-    model.add(ZeroPadding2D((1, 1)))
-
-    model.add(Conv2D(512, (3, 3), activation='relu'))
-
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-    model.add(ZeroPadding2D((1, 1)))
-
-    model.add(Conv2D(512, (3, 3), activation='relu'))
-
-    model.add(ZeroPadding2D((1, 1)))
-
-    model.add(Conv2D(512, (3, 3), activation='relu'))
-
-    model.add(ZeroPadding2D((1, 1)))
-
-    model.add(Conv2D(512, (3, 3), activation='relu'))
-
-    model.add(MaxPooling2D((2, 2), strides=(2, 2)))
-
-    model.add(Flatten())
-
-    model.add(Dense(4096, activation='relu'))
-
-    model.add(Dropout(0.5))
-
-    model.add(Dense(4096, activation='relu'))
-
-    model.add(Dropout(0.5))
-
-    model.add(Dense(1000, activation='softmax'))
-
-    if weights_path:
-        model.load_weights(weights_path)
-
-    return model
-
-
 if __name__ == '__main__':
-    print('tf version', tf.__version__)
-    print('keras version', keras.__version__)
 
-    '''
-    test inner vgg16 model
-    '''
-    # vgg16_inner = vgg16_inner()
-    # print('Inner Model loaded.')
-    # vgg16_inner.summary()
+    vgg16_inner = vgg16Inner()
+    print('Inner Model loaded.')
+    vgg16_inner.summary()
 
-    '''
-    test sequential vgg16 model
-    '''
-    # from keras import backend as K
-    #
-    # K.set_image_dim_ordering('th')
-    # vgg16_seq = vgg16_seq()
-    # print('Sequential Model loaded.')
-    # vgg16_seq.summary()
-    #
-    # model = vgg16_seq('vgg16_weights.h5')
+    train_datagen = ImageDataGenerator(
+        rescale=1. / 255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True)
+
+    # only rescaling for testing
+    test_datagen = ImageDataGenerator(rescale=1. / 255)
+    batchSize = FLAGS.BatchSize
+    targetSize = (FLAGS.VGGImageHeight, FLAGS.VGGImageWidth)
+
+    # 优化器 rmsprop：除学习率可调整外，建议保持优化器的其他默认参数不变
     # sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
-    # model.compile(optimizer=sgd, loss='categorical_crossentropy',metrics=['accuracy'])
 
-    # 模型训练
-    # model_vgg_mnist_pretrain.fit(X_train, y_train_one, validation_data=(X_test, y_test_one),
-    #                              epochs=200, batch_size=128)
+    if FLAGS.NumOfClass > 2:
+        vgg16_inner.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+        train_generator = train_datagen.flow_from_directory(FLAGS.TrainData, target_size=targetSize,
+                                                            batch_size=batchSize, class_mode='categorical')
+        validation_generator = test_datagen.flow_from_directory(FLAGS.ValidationData, target_size=targetSize,
+                                                                batch_size=batchSize, class_mode='categorical')
+    else:
+        vgg16_inner.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])  # for 2 class
+        train_generator = train_datagen.flow_from_directory(FLAGS.TrainData, target_size=targetSize,
+                                                            batch_size=batchSize, class_mode='binary')
+        validation_generator = test_datagen.flow_from_directory(FLAGS.ValidationData, target_size=targetSize,
+                                                                batch_size=batchSize, class_mode='binary')
 
+    # train model
+    # vgg16_inner.fit(X_train, y_train_one, validation_data=(X_test, y_test_one), epochs=200, batch_size=128)
+
+    vgg16_inner.fit_generator(train_generator, samples_per_epoch=2000, nb_epoch=500,
+                              validation_data=validation_generator,
+                              nb_val_samples=800)
+    vgg16_inner.save_weights('first_try.h5')  # save weights
